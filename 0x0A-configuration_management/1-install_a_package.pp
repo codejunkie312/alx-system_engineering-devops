3b780383653de5d93d0604cd6ff3be8c6d3ec288
codejunkie312 <anas.sahimi1@gmail.com>
2023-12-22 14:58:12 -0800

    Fixed task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
-# install flint from pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
+# install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
-  require  => Package['python3-pip'],
+}
+
+# instal Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
 }
