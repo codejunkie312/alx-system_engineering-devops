fa15adbb72eeb7249c3dc2bea13384b4a76e501b
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-02-19 02:13:55 +0100

    added task0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fixes the issue
+exec { 'fix_issue':
+  command => 'sed -i "s|.phpp|.php|g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+}
